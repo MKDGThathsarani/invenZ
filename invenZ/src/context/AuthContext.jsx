fc6d08d7fd350549886db7b277185369fec99ccf
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-08-04 00:36:35 +0530

    fix error

--- invenZ/src/context/AuthContext.jsx
+++ invenZ/src/context/AuthContext.jsx
@@ -433,7 +433,7 @@ export const AuthProvider = ({ children }) => {
 
   return (
     <AuthContext.Provider value={value}>
-      {!loading && children}
+      {children}
     </AuthContext.Provider>
   );
 };
